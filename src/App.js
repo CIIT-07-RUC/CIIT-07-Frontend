be856eb562bb6321b63e2ac7a304edf618c191e7
Botond Horvath <hello@boti7.com>
2023-12-07 20:20:59 +0100

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { IndexPage } from './pages/IndexPage';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <IndexPage/>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export function Footer(props) {
+	return (
+    <>
+      <div className="footer">
+        <span>Copyrights 2024 || CIIT-07</span>
+      </div>
+    </>
+	);
+}
