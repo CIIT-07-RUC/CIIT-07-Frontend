95bb556b13f69106a12960adbb59614576c8f6b5
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-10 19:49:52 +0100

    Movie list component

---
+++ src/components/movieList/MovieList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import img1 from '../../assets/images/movie2.jpeg';
+import './index.scss';
+
+const MovieList = (props) => {
+
+console.log("props.movies", props.movies)
+	const movieGrid = 12 / (props.movies).length;
+	console.log("movieGrid", movieGrid)
+  return (
+    <Container className='bg-light movie-list__component'>
+      <h2 className='text-center pb-3 pt-3'>Known for movies</h2>
+      <Row>
+        {props.movies.map((movie) => (
+          <Col key={movie.id} md={ movieGrid <= 6 ? movieGrid * 2  : movieGrid}  lg={movieGrid}>
+            <Card>
+              <Card.Img variant="top" src={movie.image} alt={movie.name} />
+              <Card.Body>
+                <Card.Title>{movie.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default MovieList;
