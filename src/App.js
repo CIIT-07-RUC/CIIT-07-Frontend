20944b4c5f5067009614eb8a06d7bfae99724cba
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-10 13:21:54 +0100

    basic structure for ThemeContext

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.scss';
 import { IndexPage } from './pages/IndexPage';
 import { Footer } from './components/footer/Footer';
 import { NavigationMain } from './components/navigation/NavigationMain';
+import React, { createContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  return (
+    <ThemeContext.Provider >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider>
 			<NavigationMain offCanvasPlacement="end" />
       <IndexPage/>
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
