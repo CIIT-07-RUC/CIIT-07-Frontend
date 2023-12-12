4e502ecbba4cb2414cf1e626635496d9383bc251
Christian0214 <100770521+RichardDickens@users.noreply.github.com>
2023-12-12 15:29:30 +0100

    New SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,27 +2,61 @@ import { NavigationBar} from '../components/navigation/NavigationBar';
 import { CCarousel } from '../components/carousel/Carousel';
 import { Row, Container, Col   } from 'react-bootstrap';
 import img1 from '../assets/images/404_image.png';
+import { NavigationMain } from '../components/navigation/NavigationMain';
+import { Footer } from '../components/footer/Footer';
 
 
+export function SearchPage(props) {
 
-export function SearchPage() {
+	const mockedSearchQuery = "leonardo";
+	
 	const mockedMovieObj = {
 		items: {
 			img: [img1, img1, img1],
 			title: ["Title 1", "Title 2", "Title 3"],
-			description: ["Lorem ipsum dolor sit amet1", "Lorem ipsum dolor sit amet2", "Lorem ipsum dolor sit amet3"]
+			description: ["Lorem ipsum dolor sit amet1", "Lorem ipsum dolor sit amet2", "Lorem ipsum dolor sit amet3"],
 		}
 	}
-	
 
-	return (
-	  <div className="">
+	const mockedPersonObj = {
+		items: {
+			name: ["Leonardo DiCaprio"],
+			proffesion: ["Actor"],
+			birthYear: ["1974"]
+		}
+	}
+
+	return (	
+		<>
+		<NavigationMain offCanvasPlacement="end" />
+		<div className="SearchPageHeader">
 			<Container>
-				<Row style={{margin: '50px 0'}}>
-					<Col><CCarousel items={mockedMovieObj} title="Fresh movies" /></Col>
-					
-				</Row>
+				<div className="search_page--page-title">
+					<h1>Search: "{mockedSearchQuery}" </h1>
+				</div>
 			</Container>
-	  </div>
+
+		<div className="Title_Results">
+			<Container>
+				<div className="search__page--title-results bg-light">
+					<h1>Titles: </h1>
+					<p> result_1: <strong> Title </strong> (year: 2010)</p>
+					<p> result_2: <strong> Title </strong> (year: 2000)</p>
+					<p> result_3: <strong> Title </strong> (year: 1998)</p>
+				</div>
+			</Container>
+		</div>
+
+		<div className="Person_Results">
+			<Container>
+				<div className="search__page--person-results bg-light">
+					<h1>People: </h1>
+					<p> result_1: <strong> Name </strong> (birth year: 1974)</p>
+				</div>
+			</Container>
+		</div>
+	</div>
+	<Footer/>
+	</>
 	);
 }
