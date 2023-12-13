dac3d6e6dd41c7f6d77d46b2fbadf1aebcc79872
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-13 17:10:53 +0100

    Footer styling + add it to bottom of page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './index.scss';
 export function Footer(props) {
 	return (
     <>
-      <div className="footer">
+      <div className="footer bg-dark" >
         <span>Copyrights 2024 || CIIT-07</span>
       </div>
     </>
