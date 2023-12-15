58e997ced65ebd4f59c8ea2aa5fdae4654941f9e
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-15 21:00:49 +0100

    Finishing Index Page with dynamic data + fixing Carousel component

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -2,31 +2,27 @@ import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import img1 from '../../assets/images/movie2.jpeg';
 import './index.scss';
+import { Button } from 'react-bootstrap';
 
 export function CCarousel (props) {
-	const items = props.items.items.img.map((img, index) => ({
-		img: props.items.items.img[index],
-		title: props.items.items.title[index],
-		description: props.items.items.description[index],
-	}));
-
 	return(
 		<>
-			<h2 className='carousel__title'>{ props.title }</h2>
+			<h4 className='carousel__title'>{ props.title }</h4>
 			<Carousel className='carousel__component' data-bs-theme="dark">
 				{
-					(Object.keys(items)).map((key) => (
+					props.items.map((el, key) => (
 						<Carousel.Item className='carousel__item'>
 							<Card className='card__item'>
 								<Card.Img 
 								className="card__item--image"
 								variant="top" 
-								src={items[key].img} />
+								src={el.poster} />
 								<Card.Body className="card__body">
-									<Card.Title>{items[key].title}</Card.Title>
+									<Card.Title >{el.primaryTitle}</Card.Title>
 									<Card.Text>
-										{items[key].description}
+										{el.plot}
 									</Card.Text>
+									<Button href={`/movie/${el.tConst}`} >Check movie</Button>
 								</Card.Body>
 								
 							</Card>
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -4,17 +4,55 @@ import { Row, Container, Col   } from 'react-bootstrap';
 import img1 from '../assets/images/movie2.jpeg';
 import img2 from '../assets/images/movie1.webp';
 import { Footer } from '../components/footer/Footer';
+import React, {useEffect, useState} from 'react';
+import { MovieAPI } from '../apis/MovieAPI';
 
 
 export function IndexPage() {
-	const mockedMovieObj = {
-		items: {
-			img: [img2, img1, img1],
-			title: ["Title 1", "Title 2", "Title 3"],
-			description: ["Lorem ipsum dolor sit amet1", "Lorem ipsum dolor sit amet2", "Lorem ipsum dolor sit amet3"]
+
+	const freshMoviesArr = ['tt26316979', 'tt23952060', 'tt22207772']
+	const recommendedMoviesArr = ['tt4285496', 'tt4434004', 'tt4846340 '];
+	const [areMoviesLoaded, setAreMoviesLoaded] = useState(false);
+	const [recommendedMovies, setRecommendedMovies] = useState([]);
+	const [freshMovies, setFeshMovies] = useState([]);
+
+	useEffect(() => {
+			fetchMovie();
+		if (areMoviesLoaded) {
+			console.log("recommendedMovies", recommendedMovies)
+		}
+	}, [areMoviesLoaded]);
+
+	const fetchMovie = async () => {
+		try {
+
+			const recommendedMoviesData = await Promise.all(
+				recommendedMoviesArr.map(async (el) => {
+				  const castMovie = await MovieAPI.getById(el);
+				  return castMovie;
+				})
+			);
+
+			const freshMoviesData = await Promise.all(
+				freshMoviesArr.map(async (el) => {
+					const castMovie = await MovieAPI.getById(el);
+					return castMovie;
+				})
+			);
+			
+			setRecommendedMovies([ ...recommendedMoviesData]);
+			setFeshMovies([ ...freshMoviesData]);
+			setAreMoviesLoaded(true);
+
+
+		} catch (e) {
+			
 		}
 	}
-	
+
+
+
+
 
 	return (
 		<>
@@ -22,8 +60,13 @@ export function IndexPage() {
 			<div className="">
 					<Container>
 						<Row style={{margin: '50px 0'}}>
-							<Col><CCarousel items={mockedMovieObj} title="Fresh movies" /></Col>
-							<Col><CCarousel items={mockedMovieObj} title="Fresh actors" /></Col>
+							{ areMoviesLoaded ?
+							<Col><CCarousel items={recommendedMovies} title="Recommended movies by us" /></Col>
+							: null }
+
+							{ areMoviesLoaded ?
+							<Col><CCarousel items={freshMovies} title="Fresh movies from this year" /></Col>
+							: null }
 						</Row>
 					</Container>
 			</div>
