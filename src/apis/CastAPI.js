ba44d4097cadce4c5eee2e5e0b216d90803e9c29
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-16 01:17:32 +0100

    Search-Coplayer API

--- src/apis/CastAPI.js
+++ src/apis/CastAPI.js
@@ -5,6 +5,11 @@ export class CastAPI {
     const response = await api.get('/cast/' + id);
     return response.data;
   }
+
+  static async getAllCoplayers(castKeyword) {
+    const response = await api.get('/cast/coplayers/' + castKeyword);
+    return response.data;
+  }
 }
 
 export default CastAPI;
