6a76bb952c45103d83773630336551c00dbb5e07
Botond Horvath <hello@boti7.com>
2023-12-16 23:57:40 +0100

    Add Rating component

---
+++ src/components/rating/Rating.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {  Star, StarFill } from 'react-bootstrap-icons';
+import './index.scss';
+
+export function Rating({ value, onClick }) {
+  const [ currentStar, setCurrentStar ] = useState(null);
+  const roundedRating = currentStar || Math.round(value);
+  const stars = Array(10).fill(false).fill(true, 0, roundedRating);
+  const clickable = !value && !!onClick;
+
+  const handleStarClick = index => {
+    if (value || !onClick) return;
+    onClick(index);
+    setCurrentStar(null);
+  };
+
+  return (
+    <div className={"rating-container " + (clickable ? 'input' : '')}>
+      {stars.map((value, index) => value
+        ? <StarFill
+          key={'ratingstar-' + index}
+          onMouseEnter={() => clickable && setCurrentStar(index + 1)}
+          onMouseLeave={() => setCurrentStar(null)}
+          onClick={() => handleStarClick(index + 1)}
+        />
+        : <Star
+          key={'ratingstar-' + index}
+          onMouseEnter={() => clickable && setCurrentStar(index + 1)}
+          onMouseLeave={() => setCurrentStar(null)}
+          onClick={() => handleStarClick(index + 1)}
+        />)}
+      {value ? <span className="ms-1">{value}</span> : null}
+    </div>
+  );
+}
+
+export default Rating;
