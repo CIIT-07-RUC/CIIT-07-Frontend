ea97ddf08b13e99342bfd528f4f1d1997fa3d8d2
Christian0214 <100770521+RichardDickens@users.noreply.github.com>
2023-12-17 13:37:36 +0100

    Update TitleList.jsx

--- src/components/searchResultsList/TitleList.jsx
+++ src/components/searchResultsList/TitleList.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Image, Button, Text } from 'react-bootstrap';
 import img1 from '../../assets/images/movie2.jpeg';
 import './index.scss';
 
 const TitleList = (props) => {
-    console.log("props.movies", props.movies)
+  console.log("props.movies", props.movies)
 	const movieGrid = 12 / (props.movies).length;
 	console.log("movieGrid", movieGrid)
+  
   return (
     <Container className='bg-light movie-list__component'>
       
-      <Row>
+      <Col>
         {props.movies.map((movie) => (
-          <Col key={movie.id} md={ movieGrid <= 6 ? movieGrid * 2  : movieGrid}  lg={movieGrid}>
+          <Col key={movie.id} sm={5} md={3}> 
             <Card>
-              
               <Card.Body>
-                <Card.Title>{movie.name}</Card.Title>
+                <Card.Title>{movie.name}  </Card.Title>
+                <Card.Text /> {movie.year} <Card.Text/>
+                <Card.Img  src={movie.poster} className="poster-picture"/>
+                <Button href={`/movie/${movie.tConst}`} >Check movie</Button>
               </Card.Body>
             </Card>
           </Col>
         ))}
-      </Row>
+      </Col>
     </Container>
   );
 };
